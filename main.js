91dcec0943cda512c473febd31a08ece39ba2351
julianmedwards <julian.michael.edwards@gmail.com>
2022-11-04 15:04:01 -0500

    Test for API requests.

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 'use strict'
+
+$(document).ready(init())
+
+function init() {
+    const API = {}
+    $.when(
+        $.getJSON('./config.json', function (data) {
+            API.key = data.tmdbAPIKey
+            API.RAToken = data.tmdbReadAccessToken
+        }).fail(function () {
+            console.error('Failed to load config info needed for API calls!')
+        })
+    ).then(testAPIRequest(API))
+}
+
+function testAPIRequest(API) {
+    $.get({
+        url: `https://api.themoviedb.org/3/search/movie?api_key=${API.key}&language=en-US&query=fast&page=1&include_adult=false`,
+        success: (data) => {
+            console.log(data)
+        },
+        dataType: 'application/json',
+    })
+}
